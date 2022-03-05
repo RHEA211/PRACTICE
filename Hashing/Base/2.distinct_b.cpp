038aa5f888ae07be419acf030a973f7a1fa8ece7
Rhea <64091188+RHEA211@users.noreply.github.com>
2022-03-05 22:28:44 +0530

    Add files via upload

---
+++ Hashing/Base/2.distinct_b.cpp
@@ -0,0 +1,44 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+int countdist(int arr[],int n) {
+
+    unordered_set<int> s(arr,arr+n);
+    
+
+    return s.size();
+}
+
+
+
+ int main()
+{
+    int n,i;
+    cout<<"elements  : ";
+    cin>>n;
+   int arr[n];
+   cout<<" Array-";
+   for(int i=0;i<n;i++)
+   {
+        cin>>arr[i];
+    }
+
+    cout<<" no. of dictinct elements are - "<<countdist(arr,n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
